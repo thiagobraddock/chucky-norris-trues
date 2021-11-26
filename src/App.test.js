686ffc7800b9e4549a21e8d03c4d60489551a146
thiago braddock <braddock01@hotmail.com>
2021-11-26 12:00:27 -0300

    Commit Inicio ⚡

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Exibe o título "Piadas do Chuck Norris" na tela', () => {});
+
+test('Exibe o corpo da piada na tela', () => {});
